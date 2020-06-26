fedf076724b517f683ec9e5ccbea05951e09402f
Rgonzalez717 <61915917+Rgonzalez717@users.noreply.github.com>
2020-06-26 15:41:23 +0700

    Class induk karyawan
     dengan tambahan property tipe karyawan

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_dan_collection.ClassInduk
+{
+    abstract class Karyawan
+    {
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+        public string tipe_karyawan { get; set; }
+
+        public abstract int Gaji();
+
+    }
+}
